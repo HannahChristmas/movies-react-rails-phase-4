6b2b6aa3773bb6a559e26834374ffd7f280d64a3
HannahChristmas <hc109909@gmail.com>
2022-12-30 12:45:50 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
   resources :users
   resources :movies
   # Routing logic: fallback requests for React Router.
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  # post '/new', to: 'movies#create'
+  # get '/mymovies', to: 'movies#my_movies'
+  
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
