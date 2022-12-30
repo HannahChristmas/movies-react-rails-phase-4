bb2d8b1cf77e613b6270f50352467ff01f667f7c
HannahChristmas <hc109909@gmail.com>
2022-12-30 12:24:37 -0500

    Users controller updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    # skip_before_action :authorize, only: :create 
+
+    def index 
+        users = User.all
+        render json: users
+    end
+
+    def show 
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    #DO NOT FORGET TO ADD BACK IN THE BANG OPERATOR!!
+    def create 
+        user = User.create(user_params)
+        session[:user_id] = user.id 
+        render json: user, status: 201
+    end
+
+    private 
+
+    def user_params 
+        params.permit(:username, :password, :password_confirmation, :avatar_url)
+    end
+
 end
