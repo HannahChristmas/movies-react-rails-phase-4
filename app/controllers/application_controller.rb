9db207cebfd8469350281e0e6b9fff7c2d43ea50
HannahChristmas <hc109909@gmail.com>
2022-12-30 12:43:17 -0500

    Update sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
-  # before_action :authorize 
+  before_action :authorize 
   rescue_from ActiveRecord::RecordInvalid, with: :handle_invalid
 
   private 
 
-  # def authorize 
-  #   @current_user = User.find_by(id: session[:user_id])
-  #   render json: { errors: "Not authorized" }, status: 401 unless @current_user
-  # end
+  def authorize 
+    @current_user = User.find_by(id: session[:user_id])
+    render json: { errors: "Not authorized" }, status: 401 unless @current_user
+  end
 
   def handle_invalid(invalid)
     render json: { errors: invalid.record.errors.full_messages }, status: 422
