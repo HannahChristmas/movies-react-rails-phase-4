b000d0fc217a6b36fe87d9faee52ea7ad4e67945
HannahChristmas <hc109909@gmail.com>
2022-12-30 11:46:28 -0500

    Movie validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,9 @@ class Movie < ApplicationRecord
     has_many :reviews, dependent: :destroy 
     has_many :users, through: :reviews
 
+    validates :title, uniqueness: true, presence: true 
+    validates :genre, presence: true 
+    validates :year, presence: true, numericality: { greater_than_or_equal_to: 1895, less_than_or_equal_to: Date.today.year }
+    validates :director, presence: true
+    validates :image_url, presence: true
 end
