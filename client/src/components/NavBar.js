45fe821066e554ac0687fbdac8519480ceb310d0
HannahChristmas <hc109909@gmail.com>
2023-06-09 12:16:03 -0400

    Change navbar to hamburger nav

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { bubble as Menu } from 'react-burger-menu';
+
 
 function NavBar({ setUser }){
 
@@ -11,16 +13,30 @@ function NavBar({ setUser }){
         });
     }
 
+    // const showSettings = (event) => {
+    //     event.preventDefault();
+    //     console.log('Settings clicked!');
+    // };
+
     return(
-        <div className="header">
-            <nav>
-                <Link to ="/movies">ALL MOVIES</Link>
-                <Link to ="/my-movies">MY MOVIES</Link>
-                <Link to ="/reviews">MY REVIEWS</Link>
-                <Link to ="/new">ADD MOVIE</Link>
-                <button onClick={handleLogoutClick}>Logout</button>
-            </nav>
-        </div>
+        // <div className="header">
+        //     <nav>
+        //         <Link to ="/movies">ALL MOVIES</Link>
+        //         <Link to ="/my-movies">MY MOVIES</Link>
+        //         <Link to ="/reviews">MY REVIEWS</Link>
+        //         <Link to ="/new">ADD MOVIE</Link>
+        //         <button onClick={handleLogoutClick}>Logout</button>
+        //     </nav>
+        // </div>
+        <>
+            <Menu right>
+                <Link id="home" className="menu-item" to="/movies">all movies</Link>
+                <Link id="about" className="menu-item" to="/my-movies">my movies</Link>
+                <Link id="contact" className="menu-item" to="/my-reviews">my reviews</Link>
+                <Link id="contact" className="menu-item" to="/new">add movie</Link>
+                {/* <a onClick={ showSettings } className="menu-item--small" href="">Settings</a> */}
+            </Menu>
+        </>
     )
 }
 
